fc735a241a3da101aefc35cc6831ded151450f5e
Davlat917 <davlatsalimov936@gmail.com>
2025-07-06 08:50:31 +0500

    Downgrade

--- bin/chat_server.dart
+++ bin/chat_server.dart
@@ -1,27 +1,18 @@
 import 'dart:io';
-import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf_web_socket/shelf_web_socket.dart';
 
 void main() async {
   final clients = <WebSocket>[];
-  final messageHistory = <String>[]; // Tarixni saqlash
 
   final handler = webSocketHandler((WebSocket socket) {
     print('🟢 Client connected.');
     clients.add(socket);
 
-    // Eski xabarlarni yuborish
-    for (var msg in messageHistory) {
-      socket.add('[OLD] $msg');
-    }
-
     socket.listen((message) {
       print('📨 Message: $message');
-      messageHistory.add(message);
-
       for (var client in clients) {
-        if (client.readyState == WebSocket.open) {
+        if (client != socket && client.readyState == WebSocket.open) {
           client.add(message);
         }
       }
@@ -31,11 +22,8 @@ void main() async {
     });
   });
 
-  final pipeline = Pipeline().addMiddleware(logRequests()).addHandler(handler);
-
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final server =
-      await shelf_io.serve(pipeline, InternetAddress.anyIPv4, port);
+  final server = await shelf_io.serve(handler, InternetAddress.anyIPv4, port);
 
   print('✅ Server running at ws://${server.address.host}:${server.port}/ws');
 }
